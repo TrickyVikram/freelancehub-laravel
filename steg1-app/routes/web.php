294adcee93d19bf9320c10faf90a956170448149
Anuj <125647962+BUZZ1592003@users.noreply.github.com>
2025-10-14 09:21:23 +0530

    Add Proposal routes

--- steg1-app/routes/web.php
+++ steg1-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\ProposalController;
 use Illuminate\Support\Facades\Route;
 
 // Redirect root to jobs index
@@ -18,3 +19,9 @@ Route::get('/quick-login', [AuthController::class, 'quickLogin'])->name('quick-l
 // Job routes
 Route::resource('jobs', JobController::class);
 Route::get('/my-jobs', [JobController::class, 'myJobs'])->name('jobs.my-jobs');
+
+// Proposal routes (authenticated)
+Route::middleware('auth')->group(function () {
+    Route::get('/proposals/create', [ProposalController::class, 'create'])->name('proposals.create');
+    Route::post('/proposals', [ProposalController::class, 'store'])->name('proposals.store');
+});
